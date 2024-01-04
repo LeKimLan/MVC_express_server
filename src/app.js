af0d229c9783d97d9a95d68a27a364c0b222b5c0
Le Kim Lan <lekimlan0205@gmail.com>
2024-01-04 21:14:46 +0700

    add

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ const app = express();
 import bodyParser from 'body-parser';
 app.use(bodyParser.json());
 
+app.get('/kek', (req, res) => {
+    return res.status(200).json({
+        message: "kek"
+    })
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is on at ${process.env.HOST}`)
 })
