a2ae8816582befbd75c722f280b50eb8517cf93c
Le Kim Lan <lekimlan0205@gmail.com>
2024-01-06 17:23:35 +0700

    finish ci/cd

--- src/app.js
+++ src/app.js
@@ -7,9 +7,9 @@ const app = express();
 import bodyParser from 'body-parser';
 app.use(bodyParser.json());
 
-app.get('/test2', (req, res) => {
+app.get('/test', (req, res) => {
     return res.status(200).json({
-        message: "OK2!"
+        message: "Website OK!"
     })
 })
 
